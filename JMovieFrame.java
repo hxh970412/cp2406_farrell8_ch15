0b8c275ccd6b77d02de12bec88014bcc84e13a8d
jc445081 <xinhao.huang@my.jcu.edu.au>
2018-09-10 14:55:11 +0800

    prac_9

---
+++ JMovieFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by jc445081 on 10/09/18.
+ */
+public class JMovieFrame extends JFrame implements ActionListener {
+    JButton button1 = new JButton("show the movie");
+    JButton button2 = new JButton("Show the movie");
+    JButton button3 = new JButton("Show the movie");
+    JButton button4 = new JButton("Show the movie");
+    JLabel label1 = 
+}
